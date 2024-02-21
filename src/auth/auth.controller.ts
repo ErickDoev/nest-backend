2e320eac35a5f86e2170c41cdd4aa0cb543e702a
elliutCruz <erick.cruz@finaktiva.com>
2024-02-21 16:59:04 -0600

    segundo commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,67 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { AuthGuard } from './guard/auth.guard';
+import User from './entities/user.entity';
+import { LoginResponse } from './interfaces/login-response';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.authService.create(createUserDto);
+  }
+
+  @Post('/login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Post('/register')
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get()
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  findAll(@Request() req: Request) {
+    return this.authService.findAll();
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('/check-token')
+  async checkToken(@Request() req: Request): Promise<LoginResponse> {
+    const user = req['user'] as User;
+    return {
+      token: await this.authService.getJwt({ id: user._id }),
+      user,
+    };
+  }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.authService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.authService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.authService.remove(+id);
+  // }
+}
---
+++ src/auth/auth.service.ts
@@ -0,0 +1,100 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-auth.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import User from './entities/user.entity';
+import * as bcryptjs from 'bcryptjs';
+import { LoginDto } from './dto/login.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from './interfaces/jwt-payload';
+import { LoginResponse } from './interfaces/login-response';
+import { RegisterDto } from './dto/register.dto';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const { password, ...userData } = createUserDto;
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData,
+      });
+      await newUser.save();
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${createUserDto.email} already exist!`);
+      }
+      throw new InternalServerErrorException('Valio vrga!!!');
+    }
+  }
+
+  async register(registerDto: RegisterDto): Promise<LoginResponse> {
+    const user = await this.create(registerDto);
+    return {
+      user,
+      token: await this.getJwt({ id: user._id }),
+    };
+  }
+
+  async login(loginDto: LoginDto): Promise<LoginResponse> {
+    const { email, password } = loginDto;
+    const user = await this.userModel.findOne({ email });
+    if (!user) {
+      throw new UnauthorizedException('Not valid credentials');
+    }
+
+    if (!bcryptjs.compareSync(password, user.password)) {
+      throw new UnauthorizedException('Not valid credentials');
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: _, ...rest } = user.toJSON();
+    return {
+      user: rest,
+      token: await this.getJwt({ id: user.id }),
+    };
+  }
+
+  findAll(): Promise<User[]> {
+    return this.userModel.find();
+  }
+
+  async findUserById(id: string) {
+    const user = await this.userModel.findById(id);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, ...rest } = user.toJSON();
+    return rest;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+
+  async getJwt(payload: JwtPayload) {
+    const token = await this.jwtService.signAsync(payload);
+    return token;
+  }
+}
